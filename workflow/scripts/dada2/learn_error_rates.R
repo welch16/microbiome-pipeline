b4a5c50a8b010e1c5d8ab82bc41c622de01de4de
Rene Welch <welch16@gmail.com>
2021-07-12 16:50:34 -0500

    changed script and config to work with batches outside of snakemake

--- workflow/scripts/dada2/learn_error_rates.R
+++ workflow/scripts/dada2/learn_error_rates.R
@@ -1,45 +1,70 @@
+#!/usr/local/bin/Rscript
 
-# dada2::learnErrors wrap for condor based pipeline
-#
-# Assumes that we have paired end files, therefore, we learn two error rates
-# matrices, i.e. one for each end.
-#
-source("renv/activate.R")
-sink(snakemake@log[[1]])
+#' Wrapper around `dada2::learnErrors function
+#' For details on the meaning of the parameters use
+#' R -e `?dada2::learnErrors`
+
+#' Assumes that we have paired end files, therefore, we learn two error rates
+#' matrices, i.e. one for each end.
+
+"Learn error rates
+
+Usage:
+learn_error_rates.R [--error_rates=<matrix_file> --plot_file=<pfile>] [<filtered> ...] [--log=<logfile> --batch=<batch> --config=<cfile> --cores=<cores>]
+learn_error_rates.R (-h|--help)
+learn_error_rates.R --version
+
+Options:
+-h --help    show this screen
+--error_rates=<matrix_file>    name of the file with the learned error rates matrix
+--plot_file=<pfile>    name of the file to save the diagnostic plot
+--log=<logfile>    name of the log file [default: error_rates.log]
+--batch=<batch>    name of the batch if any to get the filter and trim parameters
+--config=<cfile>    name of the yaml file with the parameters [default: ./config/config.yaml]
+--cores=<cores>    number of CPUs for parallel processing [default: 4]" -> doc
+
+library(docopt)
+
+my_args <- commandArgs(trailingOnly = TRUE)
+
+arguments <- docopt::docopt(doc, args = my_args, version = "error_rates V1")
+
+stopifnot(any(file.exists(arguments$filtered)))
+
+if (!is.null(arguments$config)) {
+  stopifnot(file.exists(arguments$config))
+}
+
+log_file <- file(arguments$log, open = "wt")
 
 info <- Sys.info();
-message(stringr::str_c(names(info), " : ", info, "\n"))
 print(stringr::str_c(names(info), " : ", info, "\n"))
 
 message("loading packages")
 library(dada2)
 library(ggplot2)
 library(qs)
+library(yaml)
+library(fs)
 
+config <- yaml::read_yaml(arguments$config)$error_rates
+print(config)
 
-err_fwd <- dada2::learnErrors(
-  snakemake@input[["R1"]], nbases = snakemake@config[["learn_nbases"]],
-  multithread = snakemake@threads, randomize = TRUE)
-err_bwd <- dada2::learnErrors(
-  snakemake@input[["R2"]], nbases = snakemake@config[["learn_nbases"]],
-  multithread = snakemake@threads, randomize = TRUE)
+if (arguments$batch %in% names(config)) config <- config[[arguments$batch]]
 
-qs::qsave(err_fwd, file = snakemake@output[["errR1"]])
-qs::qsave(err_bwd, file = snakemake@output[["errR2"]])
+errs <- dada2::learnErrors(
+  arguments$filtered, nbases = as.numeric(config$learn_nbases),
+  multithread = as.numeric(arguments$cores), randomize = TRUE)
+  
+fs::dir_create(dirname(arguments$error_rates))
+qs::qsave(errs, file = arguments$error_rates)
 
-err_fwd_plot <- dada2::plotErrors(err_fwd, nominalQ = TRUE)
-err_bwd_plot <- dada2::plotErrors(err_bwd, nominalQ = TRUE)
+err_plot <- dada2::plotErrors(errs, nominalQ = TRUE)
 
+fs::dir_create(dirname(arguments$plot_file))
 ggplot2::ggsave(
-  filename = snakemake@output[["plotErr1"]],
-  plot = err_fwd_plot,
-  width = 20,
-  height = 20,
-  units = "cm")
-
-ggplot2::ggsave(
-  filename = snakemake@output[["plotErr2"]],
-  plot = err_bwd_plot,
+  filename = arguments$plot_file,
+  plot = err_plot,
   width = 20,
   height = 20,
   units = "cm")
